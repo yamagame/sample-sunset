f6c0e5b8837403ed98f9c6a8e35687a3291a54e3
Hiromitsu Yamaguchi <yamagame@mac.com>
2019-05-13 13:20:26 +0900

    インストールスクリプト追加

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd `dirname $0`
+#ln -s \`realpath ./Sound\` ~/Sound/sample-sunset
+ln -s \`realpath ./Pictures\` ~/Pictures/sample-sunset
+#ln -s \`realpath ./Commands\` ~/dora-engine/command/sample-sunset
